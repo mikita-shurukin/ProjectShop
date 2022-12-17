c174dfca0b3ddb01ef8eab74382aca2c96c60f37
Vortex784 <109910240+Vortex784@users.noreply.github.com>
2022-12-17 19:10:18 +0100

    Json serialize

--- Program.cs
+++ Program.cs
@@ -1,26 +1,51 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-//TEST1
-namespace Shop
+using System.Text.Json;
+using System.Net;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace JSONSerializers
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            string[] d = File.ReadAllLines("path (input)");
+            Console.WriteLine(d);
+
+            var picture = new IPicture();
+
+            for (int i = 0; i <= d.Length - 1; i++)
+            {
+                string id = d[i];
+                var a = _download_serialized_json_data<IJSON>(id);
+
+                Console.WriteLine($"{a.safe_title}");
+
+                using (WebClient webClient = new WebClient())
+                {
+                    byte[] dataArr = webClient.DownloadData($"{a.img}");
+                    File.WriteAllBytes("path (output)", dataArr);
+                }
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static T _download_serialized_json_data<T>(string id) where T : new()
+        {
+            using (var w = new WebClient())
+            {
+                var json_data = string.Empty;
+                try
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    json_data = w.DownloadString($"");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }
     }
 }
